6921016f065dcc9d4cc3e222ed4568fdf09979db
WangWei <wei.s.wang@workplace.com>
2021-03-16 21:04:47 +0800

    add new js file & implement some mobile menu function

---
+++ MyHomePage/main.js
@@ -0,0 +1,26 @@
+//点击菜单栏按钮显示菜单栏
+$("#hamburger-menu > a").on("click",
+function(e){
+    e.preventDefault();
+    $(".slide-bar").toggleClass("show");
+    $("body").addClass("on-side");
+    $('.body-overlay').addClass('active');
+    $(this).addClass('active');
+});
+//点击关闭按钮关闭菜单栏
+$(".close-mobile-menu > a").on("click",
+function(e){
+    e.preventDefault();
+    $(".slide-bar").removeClass("show");
+    $("body").removeClass("on-side");
+    $('.body-overlay').removeClass('active');
+    $('.hamburger-menu > a').removeClass('active');
+});
+//点击空白处关闭菜单栏
+$('.body-overlay').on('click',
+function(){
+    $(this).removeClass('active');
+    $(".slide-bar").removeClass("show");
+    $("body").removeClass("on-side");
+    $('.hamburger-menu > a').removeClass('active');
+});
